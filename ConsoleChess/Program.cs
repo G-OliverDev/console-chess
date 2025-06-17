8b39353d06e61c6a22642c94ea574a077990ba7a
G-OliverDev <180020076+G-OliverDev@users.noreply.github.com>
2025-06-17 17:14:37 -0300

    feat: created Position class

--- ConsoleChess/Program.cs
+++ ConsoleChess/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace ConsoleChess
+﻿using ConsoleChess.Board;
+
+namespace ConsoleChess
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            Position p;
+
+            p = new Position(3, 4);
+
+            Console.WriteLine("Position: " + p);
         }
     }
 }
